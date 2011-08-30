4886dccb14137759d8f9c04c164e774263ddfc98
Jeffrey Ness <jness@flip-edesign.com>
2011-08-30 12:42:52 -0500

    adding search back

--- mojo/mfweb/views.py
+++ mojo/mfweb/views.py
@@ -1,11 +1,11 @@
 from django.contrib.sessions.backends.file import SessionStore
 from django.shortcuts import render_to_response, render
-from django.http import HttpResponse
+from django.http import Http404
 from django.conf import settings
 from django import forms
 
-from datetime import datetime
-from urllib2 import urlopen
+from re import search
+from urllib2 import urlopen, HTTPError
 from json import loads
 
 # Monkeyfarm path
@@ -98,7 +98,40 @@ def template_sort(results):
     if results[0] in mysort:
         return mysort.index(results[0])
     else:
-        return len(mysort) + 1
+        return len(mysort) + 1 
+
+def result_dictionary(request, r, viewtype):
+    '''Creates a default result dictionary to send to our templates'''
+
+    # get a list of all MF Projects and Systems 
+    projects = get_mf_json(
+                    '%s/project/get_all.json' % (mfroot), 'label')
+    systems = get_mf_json(
+                    '%s/system/get_all.json' % (mfroot), 'label')
+   
+    resultDict = {}
+    # limit results to 50 items or less
+    if type(r) == list:
+        results = r[0:50]
+    else:
+        results = r
+
+    # create the dict which will go to Django template
+    resultDict['results'] = results
+    resultDict['project'] = get_project(request)
+    resultDict['project_list'] = projects
+    resultDict['systems'] = systems
+    resultDict['viewtype'] = viewtype
+
+    # sort label names in the proper order
+    try:
+        resultDict['results'] = sorted(results.items(), key=template_sort)
+    except AttributeError:
+        resultDict['results'] = results
+    except KeyError:
+        resultDict['results'] = results
+
+    return resultDict
 
 def get_project(request):
     '''Get our current Monkeyfarm project and save in session'''
@@ -118,307 +151,74 @@ def get_project(request):
 
     # add project to session
     request.session['project'] = project
-
     return project
 
-def date_format(resultDict):
-    '''Modify create_date to ctime()'''
-    for r in resultDict['results']:
-        r['create_date'] = datetime.strptime(r['create_date'], '%Y-%m-%d %H:%M:%S').ctime()
-    return resultDict
-
-def result_dictionary(request, r, page):
-    '''Creates a default result dictionary to send to our templates'''
-
-    # get a list of all MF Projects    
-    projects = getmf_json(
-                    '%s/project/get_all.json' % (mfroot), 'projects', 'label')
-    
-    # get all systems in MF
-    systems = getmf_json(
-                    '%s/system/get_all.json' % (mfroot), 'systems', 'label'
-    )
-    
-    resultDict = {}
-    
-    # if we give a list lets limit its data
-    # to 75 items
-    if type(r) == list:
-        results = r[0:75]
-    else:
-        results = r
-       
-    resultDict['result'] = results
-    resultDict['project'] = get_project(request)
-    resultDict['project_list'] = projects
-    resultDict['systems'] = systems
-    resultDict['page'] = page
-   
-    # sort in the proper order
-    try:
-        resultDict['results'] = sorted(results.items(), key=template_sort)
-    except AttributeError:
-        resultDict['results'] = results
-
-    return resultDict
-
-def getmf_json(url, type, order=False, rev=False):
+def get_mf_json(url, order=False, rev=False):
     '''Makes our Monkeyfarm request and parses with lib json,
     after we will pull the data and sort it by key'''
-    request = urlopen(url).read()
+    try:
+        request = urlopen(url).read()
+    except HTTPError:
+        # try removing project label
+        url = search('(.*)\?project_label=.*', url).group(1)
+        try:
+            request = urlopen(url).read()
+        except HTTPError:
+            raise Http404
+
     json = loads(request)
-    result = json['data'][type]
-        
+    result = json['data']
+    result_types = result.keys()
+
+    # determine the API call type
+    for t in result_types:
+        result_type = t
+    data_results = result[result_type]
+
+    # perform sorting if set
     if order:
-        # order the data by key    
-        result.sort(key=lambda x: x[order], reverse=rev)
-    return result
+        try:
+            # order the data by key    
+            data_results.sort(key=lambda x: x[order], reverse=rev)
+        except AttributeError:
+            pass
+        except KeyError:
+            pass
+
+    return data_results
 
 #
 # Views tied directly to web templates
 #
 
-def builds(request):
+def mf_request(request, viewtype, label):
     '''Request to the root of the website will land on this function'''
     # verify our working project
     project = get_project(request)
 
-    builds = getmf_json(
-                   '%s/build/get_all.json?project_label=%s' % (mfroot, project),
-                   'builds',
-                   'create_date',
-                   True)
+    # provide differnt sort basedon viewtype
+    sorts = {}
+    sorts['build'] = 'create_date'
+    try:
+        order = sorts[viewtype]
+    except KeyError:
+        # default to label for sorting
+        order = 'label'
+
+    # check to see if this is a direct object request
+    if label:
+        s = '%s/%s/%s/get_one.json?project_label=%s' % (mfroot, viewtype, label, project)
+        page = 'infopage.html'
+    else:
+        s = '%s/%s/get_all.json?project_label=%s' % (mfroot, viewtype, project)
+        page = 'listpage.html'
+
+    results = get_mf_json(s, order, True)
+
     
     # build our results for the template    
-    resultDict = result_dictionary(request, builds, 'build')
-    
-    # change create_date to ctime()
-    resultDict = date_format(resultDict)
-    return render(request, 'listpage.html', resultDict)
-        
-def tasks(request):
-    '''Func for returning all task on a project'''
-    # verify our working project
-    project = get_project(request)
-
-    tasks = getmf_json(
-                   '%s/task/get_all.json?project_label=%s' % (mfroot, project),
-                   'tasks',
-                   'create_date',
-                   True)
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, tasks, 'task')
-    
-    # change create_date to ctime()
-    resultDict = date_format(resultDict)
-
-    return render(request, 'listpage.html', resultDict)
-
-def buildinfo(request, label):
-    '''Grab all json information from a build'''
-    # verify our working project
-    project = get_project(request)
-
-    build = getmf_json(
-                   '%s/build/%s/get_all.json?project_label=%s' % (mfroot, label, project),
-                   'build')
-    
-    # get status of a task for template
-    t_status = []
-    for task in build['tasks']:
-        taskinfo = getmf_json(
-                   '%s/task/%s/get_all.json?project_label=%s' % (mfroot, task, project),
-                   'task')
-        status = taskinfo['status_label']
-        t_status.append((task, status))
-    build['tasks'] = t_status
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, build, 'build')
-    return render(request, 'infopage.html', resultDict)
-
-def taskinfo(request, label):
-    '''Grab all json information from a task'''
-    # verify our working project
-    project = get_project(request)
-
-    task = getmf_json(
-                   '%s/task/%s/get_all.json?project_label=%s' % (mfroot, label, project),
-                   'task')
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, task, 'task')
-    return render(request, 'infopage.html', resultDict)
-
-def packages(request):
-    '''Return all packages in a project'''
-    # verify our working project
-    project = get_project(request)
-
-    packages = getmf_json(
-                    '%s/package/get_all.json?project_label=%s' % (mfroot, project),
-                    'packages')  
-
-    # build our results for the template    
-    resultDict = result_dictionary(request, packages, 'package')
-    return render(request, 'listpage.html', resultDict)
-    
-def packageinfo(request, label):
-    '''Grab all json information from a package'''
-    # verify our working project
-    project = get_project(request)
-
-    package = getmf_json(
-                   '%s/package/%s/get_all.json?project_label=%s' % (mfroot, label, project),
-                   'package')
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, package, 'package')
-    return render(request, 'infopage.html', resultDict)
-    
-def users(request):
-    '''Return all users registered in Monkeyfarm'''
-    # verify our working project
-    project = get_project(request)
-
-    users = getmf_json(
-                    '%s/user/get_all.json' % (mfroot),
-                    'users')  
-
-    # build our results for the template    
-    resultDict = result_dictionary(request, users, 'user')
-    return render(request, 'listpage.html', resultDict)
-    
-def userinfo(request, label):
-    '''Return all json information from a user'''
-    # verify our working project
-    project = get_project(request)
-
-    user = getmf_json(
-                    '%s/user/%s/get_all.json' % (mfroot, label),
-                    'user')  
-
-    # build our results for the template    
-    resultDict = result_dictionary(request, user, 'user')
-    return render(request, 'infopage.html', resultDict)
-    
-def groups(request):
-    '''Return all groups registered in Monkeyfarm'''
-    # verify our working project
-    project = get_project(request)
-
-    groups = getmf_json(
-                    '%s/group/get_all.json' % (mfroot),
-                    'groups')   
-        
-    # build our results for the template    
-    resultDict = result_dictionary(request, groups, 'group')
-    return render(request, 'listpage.html', resultDict)
-    
-def groupinfo(request, label):
-    '''Return all json information from a user'''
-    # verify our working project
-    project = get_project(request)
-
-    group = getmf_json(
-                    '%s/group/%s/get_all.json' % (mfroot, label),
-                    'group')  
-
-    # build our results for the template    
-    resultDict = result_dictionary(request, group, 'group')
-    return render(request, 'infopage.html', resultDict)
-    
-def tags(request):
-    '''Return all tags in a project'''
-    # verify our working project
-    project = get_project(request)
-
-    tags = getmf_json(
-                    '%s/tag/get_all.json?project_label=%s' % (mfroot, project),
-                    'tags')   
-        
-    # build our results for the template    
-    resultDict = result_dictionary(request, tags, 'tag')
-    return render(request, 'listpage.html', resultDict)
-    
-def taginfo(request, label):
-    '''Grab all json information from a tag'''
-    # verify our working project
-    project = get_project(request)
-
-    tag = getmf_json(
-                   '%s/tag/%s/get_all.json?project_label=%s' % (mfroot, label, project),
-                   'tag')
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, tag, 'tag')
-    return render(request, 'infopage.html', resultDict)
-
-def packagebranchinfo(request, label):
-    '''Grab all json information from a tag'''
-    # verify our working project
-    project = get_project(request)
-
-    packagebranch = getmf_json(
-                   '%s/package_branch/%s/get_all.json?project_label=%s' % (mfroot, label, project),
-                   'package_branch')
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, packagebranch, 'packagebranch')
-    return render(request, 'infopage.html', resultDict)
-
-def targets(request):
-    '''Return all tags in a project'''
-    # verify our working project
-    project = get_project(request)
-
-    targets = getmf_json(
-                    '%s/target/get_all.json' % (mfroot),
-                    'targets')   
-        
-    # build our results for the template    
-    resultDict = result_dictionary(request, targets, 'target')
-    return render(request, 'listpage.html', resultDict)
-    
-def targetinfo(request, label):
-    '''Grab all json information from a tag'''
-    # verify our working project
-    project = get_project(request)
-
-    target = getmf_json(
-                   '%s/target/%s/get_all.json' % (mfroot, label),
-                   'target')
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, target, 'target')
-    return render(request, 'infopage.html', resultDict)
-
-def systems(request):
-    '''Return all systems'''
-    # verify our working project
-    project = get_project(request)
-
-    systems = getmf_json(
-                    '%s/system/get_all.json' % (mfroot),
-                    'systems')  
-
-    # build our results for the template    
-    resultDict = result_dictionary(request, systems, 'system')
-    return render(request, 'listpage.html', resultDict)
-    
-def systeminfo(request, label):
-    '''Grab all json information from a package'''
-    # verify our working project
-    project = get_project(request)
-
-    system = getmf_json(
-                   '%s/system/%s/get_all.json' % (mfroot, label),
-                   'system')
-    
-    # build our results for the template    
-    resultDict = result_dictionary(request, system, 'system')
-    return render(request, 'infopage.html', resultDict)
+    resultDict = result_dictionary(request, results, viewtype)
+    return render(request, page, resultDict)
 
 def search(request):
     '''Handle searches from the web interface'''
@@ -432,13 +232,14 @@ def search(request):
         except KeyError:
             return builds(request)
         else:
-            
-            url = '%s/%s/get_all.json?project_label=%s&mf_search_query=%s' % (
-                                    mfroot, search_type, project, search_word)
-            
-            results = getmf_json(url, search_type+'s', 'create_date', True)
-            
+
+            url = '%s/%s/get_all.json?mf_search_query=%s&project_label=%s' % (
+                                    mfroot, search_type, search_word, project)
+
+            results = get_mf_json(url, search_type, False)
+
             # build our results for the template    
             resultDict = result_dictionary(request, results, search_type)
-            return render(request, search_type+'s.html', resultDict)
-        
+            return render(request, 'listpage.html', resultDict)
+            
+
--- mojo/urls.py
+++ mojo/urls.py
@@ -1,57 +1,13 @@
-
 from django.conf.urls.defaults import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('',
 
     # Root Index and Build 
-    (r'^$', 'mfweb.views.builds'),
-    (r'^builds/$', 'mfweb.views.builds'),
-    (r'^buildinfo/(\S+)$', 'mfweb.views.buildinfo'),
-
-    # Task Index page
-    (r'^tasks/$', 'mfweb.views.tasks'),
-    (r'^taskinfo/(\S+)$', 'mfweb.views.taskinfo'),
-
-    # MF Users
-    (r'^users/$', 'mfweb.views.users'),
-    (r'^userinfo/(\S+)$', 'mfweb.views.userinfo'),
-
-    # MF Groups
-    (r'^groups/$', 'mfweb.views.groups'),
-    (r'^groupinfo/(\S+)$', 'mfweb.views.groupinfo'),
-
-    # MF tags
-    (r'^tags/$', 'mfweb.views.tags'),
-    (r'^taginfo/(\S+)$', 'mfweb.views.taginfo'),
-    
-    # MF targets
-    (r'^targets/$', 'mfweb.views.targets'),
-    (r'^targetinfo/(\S+)$', 'mfweb.views.targetinfo'),
-    
-    # MF package branch
-    (r'^packagebranchinfo/(\S+)$', 'mfweb.views.packagebranchinfo'),
-
-    # Projects Packages
-    (r'^packages/$', 'mfweb.views.packages'),
-    (r'^packageinfo/(\S+)$', 'mfweb.views.packageinfo'),
-    
-    # Systems
-    (r'^systems/$', 'mfweb.views.systems'),
-    (r'^systeminfo/(\S+)$', 'mfweb.views.systeminfo'),
-
-    # Searching 
+    (r'^$', redirect_to, {'url': '/build/'}),
     (r'^search/$', 'mfweb.views.search'),
-
-    # info pages without a search term
-    (r'^buildinfo/$', 'mfweb.views.builds'),
-    (r'^taskinfo/$', 'mfweb.views.tasks'),
-    (r'^userinfo/$', 'mfweb.views.users'),
-    (r'^groupinfo/$', 'mfweb.views.groups'),
-    (r'^taginfo/$', 'mfweb.views.tags'),
-    (r'^packagebranchinfo/$', 'mfweb.views.packages'),
-    (r'^targetinfo/$', 'mfweb.views.targets'),
-    (r'^systeminfo/$', 'mfweb.views.systems'),
+    (r'^(\S+)/(\S+)?$', 'mfweb.views.mf_request'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
