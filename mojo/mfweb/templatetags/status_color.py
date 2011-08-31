ed9105c0326b9a0dfe9118ebb709ad4d8013396d
Jeffrey Ness <jness@flip-edesign.com>
2011-08-31 12:38:22 -0500

    need to have a KeyError except

--- mojo/mfweb/templatetags/status_color.py
+++ mojo/mfweb/templatetags/status_color.py
@@ -5,9 +5,12 @@ register = template.Library()
 @register.filter
 
 def status_color(label, r):
-    if r['status_label'] == 'BuildSuccess':
-        return '<font color="#00CC00">%s</font>' % label
-    elif r['status_label'] == 'BuildFail':
-        return '<font color="red">%s</font>' % label
-    else:
+    try:
+        if r['status_label'] == 'BuildSuccess':
+            return '<font color="#00CC00">%s</font>' % label
+        elif r['status_label'] == 'BuildFail':
+            return '<font color="red">%s</font>' % label
+        else:
+            return label
+    except KeyError:
         return label
